4f3e3158053c59b9cc4945b6e06a4ebc6b2db4a8
Abraham Sibaja <canabraham69@gmail.com>
2019-07-10 12:11:55 -0600

    Test1: Removed www.cana.hulilabs.xyz

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(cana.hulilabs.xyz www.cana.hulilabs.xyz)
+domains=(cana.hulilabs.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="abraham.sibaja@hulilabs.com" # Adding a valid address is strongly recommended
